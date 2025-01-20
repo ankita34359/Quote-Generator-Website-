44d9be217044f36130ace01548d63a7794323acd
Ankita Gupta <120979026+ankita34359@users.noreply.github.com>
2025-01-20 13:33:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,7 @@ getQuote(api_url);
 function tweet(){
     window.open("https://twitter.com/intent/tweet?text=" + quote.innerHTML + "----by " + author.innerHTML, "Tweet Window", "width=600, height=300");
 }
+
+function linkedin() {
+    window.open("https://www.linkedin.com/feed/?shareActive=true&text=" + quote.innerHTML + "----by " + author.innerHTML, "linkedin Window", "width=600, height=300" );
+}
